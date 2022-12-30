e6e7389097644b1826c0bb52b48885302d36fc19
Troye Jian <troyej112@gmail.com>
2022-12-30 16:47:00 +0800

    Add servers in openapi

--- src/main/java/swe/troye/MovieApplication.java
+++ src/main/java/swe/troye/MovieApplication.java
@@ -4,11 +4,12 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.ws.rs.core.Application;
 
-@OpenAPIDefinition(tags = {
-                @Tag(name = "movies", description = "Operations related to movies") }, info = @Info(title = "Movie APIs", version = "1.0.0", license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html")))
+@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8081") }, tags = {
+        @Tag(name = "movies", description = "Operations related to movies") }, info = @Info(title = "Movie APIs", version = "1.0.0", license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html")))
 public class MovieApplication extends Application {
 
 }
